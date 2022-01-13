b2f365ff0cefb47539475711f6f2f764196cc26e
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2022-01-13 19:34:33 +0530

    Updated Dashboard

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,24 +1,32 @@
-import { FilterIcon, PlusIcon, SortDescendingIcon } from "@heroicons/react/outline";
+import { PlusIcon } from "@heroicons/react/outline";
 import Header from "../components/Header";
 import RecordCard from "../components/RecordCard";
 
 function DashboardPage() {
+  function addRecord() {}
+
+  let records = [
+    { title: "Test Record 1", date: "25-12-2021" },
+    { title: "Test Record 2", date: "30-12-2021" },
+    { title: "Test Record 3", date: "01-01-2022" },
+    { title: "Test Record 4", date: "02-01-2022" },
+  ];
+
   return (
     <div className="bg-neutral-800 h-full">
       <Header />
-			<div className="text-white flex justify-end p-3">
-				<button className="p-2 mr-4"><SortDescendingIcon className="h-6 w-6" /></button>
-				<button className="p-2 mr-4"><FilterIcon className="h-6 w-6" /></button>
-				<button className="bg-neutral-700 py-3 pl-3 pr-4 mr-16 rounded-xl flex">
-					<PlusIcon className="h-6 w-6 mr-2" />
-					<span>Add</span>
-					</button>
-			</div>
-			<div className="grid grid-cols-2 gap-2 p-4">
-				<RecordCard title="Test Record 1" date="25-12-2021" />
-				<RecordCard title="Test Record 2" date="30-12-2021" />
-				<RecordCard title="Test Record 3" date="01-01-2022" />
-			</div>
+      <div className="text-white flex justify-end p-3">
+        <button
+          className="bg-neutral-700 py-3 pl-3 pr-4 mr-16 rounded-xl flex"
+          onClick={addRecord}
+        >
+          <PlusIcon className="h-6 w-6 mr-2" />
+          <span>Add</span>
+        </button>
+      </div>
+      <div className="grid grid-cols-2 gap-2 p-4">
+				{records.map((val) => <RecordCard title={val.title} date={val.date} />)}
+      </div>
     </div>
   );
 }
