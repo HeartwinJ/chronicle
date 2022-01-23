98c913d7bca3fc3d562ecceb5158a3fb491ae4d8
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2022-01-23 17:14:50 +0530

    Removed unnecessary log statement

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -8,7 +8,6 @@ function ProtectedRoute(props) {
 
   useEffect(() => {
     const authRefreshInterval = setInterval(() => {
-      console.log("Auth Refresh");
       if (!auth.checkAuth(false)) {
         navigate("/login", { replace: true });
       }
